db548cf4ab0c6ebb43124110d68704d210de1f94
niuzhihao <l-niuzhihao@qianxin.com>
2021-01-07 14:09:01 +0800

    feat: init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page {{ count }}</h1>
+    <button @click="changeCount">+</button>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup(props, context) {
+    const count = ref(0)
+
+    const changeCount = () => {
+      count.value++
+    }
+
+    return { count, changeCount }
+  }
+}
+</script>
